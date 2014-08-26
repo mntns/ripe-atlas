989d62105bcae4c1aa819cd362f413143f6b7975
Eddy Shure <mono@tucana.uberspace.de>
2014-08-26 22:56:13 +0200

    Fixed measurement object.

--- lib/ripe-atlas/measurement.rb
+++ lib/ripe-atlas/measurement.rb
@@ -1,8 +1,8 @@
 module Atlas
   class Measurement
-   attr_accessor :can_visualise, :creation_time, :description, :dst_addr, :dst_asn, :dst_name, :interval,
-    :is_oneoff, :is_public, :msm_id, :probe_sources, :resolve_on_probe, :resolved_ips, :result, :start_time,
-    :status, :stop_time, :type
+   attr_accessor :af, :all_scheduling_requests_fulfilled, :can_visualise, :creation_time, 
+     :description, :dst_addr, :dst_asn, :dst_name, :interval, :is_oneoff, :is_public, :msm_id, 
+     :participant_count, :resolve_on_probe, :resolved_ips, :result, :start_time, :status, :stop_time, :type
    
    # Just for Eliah Shure: (.)(.)
    def initialize(p)
