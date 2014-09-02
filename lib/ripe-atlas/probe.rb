49d86e7c92e74d26c855dc3253478d8fe8e200a6
Eddy Shure <mono@tucana.uberspace.de>
2014-09-02 22:17:10 +0200

    Fixed probe class.

--- lib/ripe-atlas/probe.rb
+++ lib/ripe-atlas/probe.rb
@@ -2,8 +2,9 @@ module Atlas
   class Probe
     
     attr_accessor :address_v4, :address_v6, :asn_v4, :asn_v6, :country_code, :id, :is_anchor, 
-      :is_public, :latitude, :longitude, :prefix_v4, :prefix_v6, :status, :status_name, :status_since    
-  
+      :is_public, :latitude, :longitude, :prefix_v4, :prefix_v6, :status, :status_name, :status_since,
+      :tags
+
     def initialize(p)
       p.each do |k,v| 
         send("#{k}=",v)
